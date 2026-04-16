3c32c288f0f07fd1934980f61bc5f35a1ac02f9e
Mikhail Lukin <21lukin21@gmail.com>
2026-04-16 13:02:08 +0500

    refactor: improve external service implementation

--- src/main/java/com/example/externalservice/controller/ExternalStudentController.java
+++ src/main/java/com/example/externalservice/controller/ExternalStudentController.java
@@ -1,19 +1,27 @@
 package com.example.externalservice.controller;
 
+import com.example.externalservice.dto.ExternalStudentRequest;
 import com.example.externalservice.dto.ExternalStudentResponse;
+import com.example.externalservice.service.ExternalStudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/external/students")
+@RequiredArgsConstructor
 public class ExternalStudentController {
 
-    @GetMapping("/{id}")
-    public ExternalStudentResponse getExtra(@PathVariable String id) {
-        return new ExternalStudentResponse(id, "extra-info-for-" + id);
-    }
+    private final ExternalStudentService externalStudentService;
 
     @PostMapping
-    public String create() {
-        return "created";
+    public ResponseEntity<ExternalStudentResponse> create(@RequestBody ExternalStudentRequest request) {
+        return ResponseEntity.ok(externalStudentService.create(request));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExternalStudentResponse> getExtra(@PathVariable String id) {
+        return ResponseEntity.ok(externalStudentService.getById(id));
+    }
+
 }
--- src/main/java/com/example/externalservice/dto/ExternalStudentRequest.java
+++ src/main/java/com/example/externalservice/dto/ExternalStudentRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ExternalStudentResponse {
+public class ExternalStudentRequest {
 
     private String extraInfo;
 }
